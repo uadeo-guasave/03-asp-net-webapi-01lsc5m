ed8b056d83055a7a2800d04d17a7ef36460a2aef
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2020-09-29 08:28:09 -0600

    14. ASP.NET Web API
    Se refactorizó el controlador de usuarios para utilizar asincronía.
    Se creó el método getbyid para obtener un usuario en específico.

--- myapi/Controllers/UserController.cs
+++ myapi/Controllers/UserController.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using myapi.Models;
 
 namespace myapi.Controllers
@@ -18,10 +20,21 @@ namespace myapi.Controllers
 
     // select * from users;
     [HttpGet]
-    public IEnumerable<User> GetAll()
+    public async Task<ActionResult<IEnumerable<User>>> GetAll()
     {
-      var users = _db.Users.Take(20).ToArray();
+      var users = await _db.Users.Take(20).ToArrayAsync();
       return users;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<User>> GetById(int id)
+    {
+      var user = await _db.Users.FindAsync(id);
+      if (user == null)
+      {
+        return NotFound();
+      }
+      return user;
     }
   }
 }
